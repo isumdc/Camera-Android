54ff1c2d654b0c1a8ffd02b49379556b1e72f82a
etbrady <etbrady@outlook.com>
2015-11-30 17:27:49 -0600

    Changed buttons to fab and menu action

--- CameraDemo/app/src/main/java/com/iastate/mobiledevelopmentclub/camerademo/MainActivity.java
+++ CameraDemo/app/src/main/java/com/iastate/mobiledevelopmentclub/camerademo/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         Button takePictureButton = (Button) findViewById(R.id.button_takePicture);
         takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,18 +53,18 @@ public class MainActivity extends AppCompatActivity {
                 onClickSelectImage(v);
             }
         });
+        */
 
 
-        /*
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onClickTakeImage(view);
             }
         });
-        */
+
     }
 
     public void onClickTakeImage(View v)
@@ -167,6 +168,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_selectPicture) {
+            onClickSelectImage(null);
             return true;
         }
 
